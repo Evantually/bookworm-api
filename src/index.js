dae94d00571f8f254605f41395659cc8d3306c35
Evan Beese <egbeesefl@gmail.com>
2018-06-09 19:11:01 -0500

    User signs up

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import dotenv from 'dotenv';
 import Promise from 'bluebird';
 
 import auth from './routes/auth';
+import users from './routes/users';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 mongoose.Promise = Promise
-mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URL);
 
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
